2f0bf1614c4e9d806235566e253a93efe5e6b015
giannizeniquel <50558959+giannizeniquel@users.noreply.github.com>
2019-10-24 13:04:48 -0300

    funcion Validar y Encriptar en Registro

--- php/clases/Registro.php
+++ php/clases/Registro.php
@@ -1,5 +1,33 @@
 <?php
     class Registro{
-        
+
+    	public function ValidarPass($pass1, $pass2){
+    		$pass1Length = strlen($pass1);
+    		$pass2Length = strlen($pass2);
+
+    		if ($pass1 == $pass2 && isset($pass1) && isset($pass2) && 
+    			$pass1Length >=6 && $pass2Length >= 6) {
+
+    			return true;
+
+    		}else{
+
+				return false;
+
+    		}
+    	}
+
+    	public function EncriptarPass($pass){
+
+    		$passEncriptado = md5($pass)
+
+    		return $passEncriptado;
+
+    	}
+
+    	public function ValidarEdad($date){
+
+    	}
+    	
     }
 ?>
